4531143d805e5311641fe1e33fdafdf57a1a5028
sasaki mai <mai.sasaki@rakus-partners.co.jp>
2022-10-27 13:43:30 +0900

    GestroomPlanのレイアウト

--- src/pages/rooms/GestroomPlan.tsx
+++ src/pages/rooms/GestroomPlan.tsx
@@ -14,7 +14,7 @@ const GestroomPlan = () => {
 
 export const RoomCard = () => {
   return (
-    <div className={RoomStyle.container}>
+    <div className={RoomStyle.roomCard}>
       <ul>
         {/* map使って回す */}
         <li>
@@ -22,24 +22,25 @@ export const RoomCard = () => {
             <h1 className={RoomStyle.roomName}>east room</h1>
             <p className={RoomStyle.roomArea}>14-15m2</p>
           </div>
-          <img className={RoomStyle.roompic} src="#" alt="roompicture" />
+          <div className={RoomStyle.detailContainer}>
+          <img className={RoomStyle.roompic} src="hotel-4.jpg" alt="roompicture" />
           <div className={RoomStyle.roomDetails}>
             <p className={RoomStyle.roomCapacity}>定員1-2名</p>
             <p className={RoomStyle.bedType}>ベッドタイプ｜セミダブル</p>
             <p className={RoomStyle.roomEquipmentTitle}>【客室室内設備】</p>
             <p className={RoomStyle.roomEquipment}>
-              32～40インチテレビ / 竹製 歯ブラシ / 歯磨き粉 / 綿棒 / コットン /
-              化粧水セット / 洗顔セット 竹製 ヘアくし / シャンプー /
+              32～40インチテレビ / 竹製 歯ブラシ / 歯磨き粉 / シャンプー /
               コンディショナー ボディーソープ / ハンドソープ / パジャマ /
               スリッパ アロマディフューザー / ヘアドライヤー / 空気清浄機 /
-              冷蔵庫 金庫 / 電気ケトル / 壁掛式Bluetoothスピーカー / Wi-Fi
+              冷蔵庫 金庫 / 電気ケトル / Wi-Fi
             </p>
-            <p className={RoomStyle.roomPrice}>¥20,000~/人</p>
+            <p className={RoomStyle.roomPrice}>¥20,000<span>~/人</span></p>
+          </div>
           </div>
           <h2 className={RoomStyle.sectionTitle}>プラン</h2>
           <hr />
           <div className={RoomStyle.planDetails}>
-            <img className={RoomStyle.planpic} src="#" alt="planpicture" />
+            <img className={RoomStyle.planpic} src="hotel-4.jpg" alt="planpicture" />
             <p className={RoomStyle.plantitle}>プラン名</p>
             <button className={RoomStyle.planReserveBtn}>このプランで予約</button>
           </div>
