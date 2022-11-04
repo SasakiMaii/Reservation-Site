4f7db9fc9cb29b27926cb6025816e6bb70180470
sasaki mai <mai.sasaki@rakus-partners.co.jp>
2022-11-04 10:20:09 +0900

    ヘッダーフッター設定

--- src/pages/rooms/SearchResults.tsx
+++ src/pages/rooms/SearchResults.tsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
 import RoomSearchSoart from "../../components/rooms/RoomSearchSoart";
 import Pageing from "../../components/rooms/Pageing";
+import Header from "../../components/layout/Header";
+import SearchResultsStyle from "../../styles/rooms/_SearchResult.module.scss";
 
 const SearchResults = () => {
   return (
     <>
-      <p>・・・空室検索</p>
-      <RoomSearchSoart/>
-      <Pageing/>
+      <Header />
+      <p className={SearchResultsStyle.searchReasults}>・・・空室検索結果</p>
+      <RoomSearchSoart />
+      <p className={SearchResultsStyle.searchReasultEmpty}>現在空室はありません</p>
+      <Pageing />
       <Link to={`/`}>ホームに戻る</Link>
     </>
   );
 };
-
 
 export default SearchResults;
