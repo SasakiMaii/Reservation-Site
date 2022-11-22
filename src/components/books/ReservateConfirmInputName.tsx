2fc6e4ca5df3d2c3876ebade0c752be916c3e5d7
kayahara mai <mai.kayahara@rakus-partners.co.jp>
2022-11-22 09:37:55 +0900

    css、firebaseデータ処理修正しました

--- src/components/books/ReservateConfirmInputName.tsx
+++ src/components/books/ReservateConfirmInputName.tsx
@@ -39,37 +39,27 @@ export const ReserveName = (props: any) => {
   );
 };
 
-export const LodgerName = (props: any) => {
-  const { lodgeFirstName, lodgeLastName, onChangeFirst, onChangeLast } = props;
+// export const LodgerName = (props: any) => {
+//   const { lodgeFirstName, lodgeLastName, onChangeFirst, onChangeLast } = props;
 
-  return (
-    <>
-      <br />
-      <input
-        type="text"
-        value={lodgeFirstName}
-        onChange={onChangeFirst}
-        placeholder="姓"
-        className={ReservateConfirmContentsStyles.input}
-      />
-      <input
-        type="text"
-        value={lodgeLastName}
-        onChange={onChangeLast}
-        placeholder="名"
-        className={ReservateConfirmContentsStyles.input}
-      />
-      <br />
-      <input
-        type="text"
-        placeholder="姓(フリガナ)"
-        className={ReservateConfirmContentsStyles.input}
-      />
-      <input
-        type="text"
-        placeholder="名(フリガナ)"
-        className={ReservateConfirmContentsStyles.input}
-      />
-    </>
-  );
-};
+
+//   return (
+//     <>
+//       <br />
+//       <input
+//         type="text"
+//         value={lodgeFirstName}
+//         onChange={onChangeFirst}
+//         placeholder="例）田中"
+//         className={ReservateConfirmContentsStyles.input}
+//       />
+//       <input
+//         type="text"
+//         value={lodgeLastName}
+//         onChange={onChangeLast}
+//         placeholder="例）太郎"
+//         className={ReservateConfirmContentsStyles.input}
+//       />
+//     </>
+//   );
+// };
